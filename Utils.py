f2b258ddc5175b9b96bd902a198d63cb64b80dc0
Carlos-Echegoyen <113899217+Carlos-Echegoyen@users.noreply.github.com>
2024-06-07 15:58:20 +0200

    Update Utils.py with resnet18 datasets

--- Utils.py
+++ Utils.py
@@ -127,7 +127,7 @@ def getDataNames():
                 'splice']
 
 
-def load_mnist_features_resnet18(with_info=False, split=False):
+def load_mnist(return_X_y=False, split=False):
     """Load and return the MNIST Data Set features extracted using a
     pretrained ResNet18 neural network (classification).
 
@@ -170,25 +170,31 @@ def load_mnist_features_resnet18(with_info=False, split=False):
     """
     module_path = dirname(__file__)
 
-    fdescr_name = join(module_path, 'descr', 'mnist_features_resnet18.rst')
-    with open(fdescr_name) as f:
-        descr_text = f.read()
+    #fdescr_name = join(module_path, 'descr', 'mnist_features_resnet18.rst')
+    #with open(fdescr_name) as f:
+    #    descr_text = f.read()
 
-    zf = zipfile.ZipFile(join(module_path, 'data',
-                              'mnist_features_resnet18_1.csv.zip'))
-    df1 = pd.read_csv(zf.open('mnist_features_resnet18_1.csv'), header=None)
-    zf = zipfile.ZipFile(join(module_path, 'data',
-                              'mnist_features_resnet18_2.csv.zip'))
-    df2 = pd.read_csv(zf.open('mnist_features_resnet18_2.csv'), header=None)
-    zf = zipfile.ZipFile(join(module_path, 'data',
-                              'mnist_features_resnet18_3.csv.zip'))
-    df3 = pd.read_csv(zf.open('mnist_features_resnet18_3.csv'), header=None)
-    zf = zipfile.ZipFile(join(module_path, 'data',
-                              'mnist_features_resnet18_4.csv.zip'))
-    df4 = pd.read_csv(zf.open('mnist_features_resnet18_4.csv'), header=None)
-    zf = zipfile.ZipFile(join(module_path, 'data',
-                              'mnist_features_resnet18_5.csv.zip'))
-    df5 = pd.read_csv(zf.open('mnist_features_resnet18_5.csv'), header=None)
+    #zf = zipfile.ZipFile(join(module_path, 'data',
+    #                          'mnist_features_resnet18_1.csv.zip'))
+    # df1 = pd.read_csv(zf.open('mnist_features_resnet18_1.csv'), header=None)
+    # zf = zipfile.ZipFile(join(module_path, 'data',
+    #                           'mnist_features_resnet18_2.csv.zip'))
+    # df2 = pd.read_csv(zf.open('mnist_features_resnet18_2.csv'), header=None)
+    # zf = zipfile.ZipFile(join(module_path, 'data',
+    #                           'mnist_features_resnet18_3.csv.zip'))
+    # df3 = pd.read_csv(zf.open('mnist_features_resnet18_3.csv'), header=None)
+    # zf = zipfile.ZipFile(join(module_path, 'data',
+    #                           'mnist_features_resnet18_4.csv.zip'))
+    # df4 = pd.read_csv(zf.open('mnist_features_resnet18_4.csv'), header=None)
+    # zf = zipfile.ZipFile(join(module_path, 'data',
+    #                           'mnist_features_resnet18_5.csv.zip'))
+    # df5 = pd.read_csv(zf.open('mnist_features_resnet18_5.csv'), header=None)
+
+    df1 = pd.read_csv(join(module_path, 'data/embedding_datasets', 'mnist_features_resnet18_train_1.csv'), header=None, index_col=0)
+    df2 = pd.read_csv(join(module_path, 'data/embedding_datasets', 'mnist_features_resnet18_train_2.csv'), header=None, index_col=0)
+    df3 = pd.read_csv(join(module_path, 'data/embedding_datasets', 'mnist_features_resnet18_train_3.csv'), header=None, index_col=0)
+    df4 = pd.read_csv(join(module_path, 'data/embedding_datasets', 'mnist_features_resnet18_train_4.csv'), header=None, index_col=0)
+    df5 = pd.read_csv(join(module_path, 'data/embedding_datasets', 'mnist_features_resnet18_test.csv'), header=None, index_col=0)
 
     dataset = np.array(pd.concat([df1, df2, df3, df4, df5]))
     data = dataset[:, :-1]
@@ -198,7 +204,8 @@ def load_mnist_features_resnet18(with_info=False, split=False):
     data = trans.fit_transform(data)
 
     target = normalizeLabels(target)
-    if not with_info:
+
+    if return_X_y:
         if split:
             # X_train, X_test, Y_train, Y_test
             X_train = data[:60000, :]
@@ -213,9 +220,9 @@ def load_mnist_features_resnet18(with_info=False, split=False):
             data = {'train': data[:60000, :], 'test': data[60000:, :]}
             target = {'train': target[:60000], 'test': target[60000:]}
         return Bunch(data=data, target=target, DESCR=descr_text)
-    return 0
 
-def load_catsvsdogs_features_resnet18(with_info=False):
+
+def load_catsvsdogs(return_X_y=False, split=False):
     """Load and return the Cats vs Dogs Data Set features extracted using a
     pretrained ResNet18 neural network (classification).
 
@@ -247,32 +254,92 @@ def load_catsvsdogs_features_resnet18(with_info=False):
     """
     module_path = dirname(__file__)
 
-    fdescr_name = join(module_path, 'descr',
-                       'catsvsdogs_features_resnet18.rst')
-    with open(fdescr_name) as f:
-        descr_text = f.read()
+    # fdescr_name = join(module_path, 'descr',
+    #                    'catsvsdogs_features_resnet18.rst')
+    # with open(fdescr_name) as f:
+    #     descr_text = f.read()
+    #
+    # zf = zipfile.ZipFile(join(module_path, 'data',
+    #                           'catsvsdogs_features_resnet18_1.csv.zip'))
+    # df1 = pd.read_csv(zf.open('catsvsdogs_features_resnet18_1.csv'))
+    # zf = zipfile.ZipFile(join(module_path, 'data',
+    #                           'catsvsdogs_features_resnet18_2.csv.zip'))
+    # df2 = pd.read_csv(zf.open('catsvsdogs_features_resnet18_2.csv'))
 
-    zf = zipfile.ZipFile(join(module_path, 'data',
-                              'catsvsdogs_features_resnet18_1.csv.zip'))
-    df1 = pd.read_csv(zf.open('catsvsdogs_features_resnet18_1.csv'))
-    zf = zipfile.ZipFile(join(module_path, 'data',
-                              'catsvsdogs_features_resnet18_2.csv.zip'))
-    df2 = pd.read_csv(zf.open('catsvsdogs_features_resnet18_2.csv'))
+    df1 = pd.read_csv(join(module_path, 'data/embedding_datasets', 'catsvsdogs_features_resnet18_1.csv'), header=None, index_col=0)
+    df2 = pd.read_csv(join(module_path, 'data/embedding_datasets', 'catsvsdogs_features_resnet18_2.csv'), header=None, index_col=0)
 
     dataset = np.array(pd.concat([df1, df2]))
+    data = dataset[:, :-1]
+    target = dataset[:, -1]
+    print(data[1,1])
+    trans = SimpleImputer(strategy='median')
+    data = trans.fit_transform(data)
+    target = normalizeLabels(target)
+
+    if return_X_y:
+        if split:
+            # X_train, X_test, Y_train, Y_test
+            X_train = data[:20000, :]
+            Y_train = target[:20000]
+            X_test = data[20000:, :]
+            Y_test = target[20000:]
+            return X_train, X_test, Y_train, Y_test
+        else:
+            return data, target
+    else:
+        if split:
+            data = {'train': data[:20000, :], 'test': data[20000:, :]}
+            target = {'train': target[:20000], 'test': target[20000:]}
+        return Bunch(data=data, target=target, DESCR=descr_text)
+
+
+def load_cifar10(return_X_y=False, split=False):
+    """Load and return the CIFAR10 Data Set features extracted using a
+    pretrained ResNet18 neural network (classification).
+    """
+    module_path = dirname(__file__)
+
+    df1 = pd.read_csv(join(module_path, 'data/embedding_datasets', 'cifar10_features_resnet18_train_1.csv'), header=None, index_col=0)
+    df2 = pd.read_csv(join(module_path, 'data/embedding_datasets', 'cifar10_features_resnet18_train_2.csv'), header=None, index_col=0)
+    df3 = pd.read_csv(join(module_path, 'data/embedding_datasets', 'cifar10_features_resnet18_train_3.csv'), header=None, index_col=0)
+    df4 = pd.read_csv(join(module_path, 'data/embedding_datasets', 'cifar10_features_resnet18_test.csv'), header=None, index_col=0)
+
+    dataset = np.array(pd.concat([df1, df2, df3, df4]))
     data = dataset[:, :-1]
     target = dataset[:, -1]
 
     trans = SimpleImputer(strategy='median')
     data = trans.fit_transform(data)
 
-    if not with_info:
-        return data, normalizeLabels(target)
+    target = normalizeLabels(target)
+    return data, target
 
-    return Bunch(data=data, target=normalizeLabels(target),
-                 DESCR=descr_text)
 
-def load_yearbook_features_resnet18(with_info=False, with_attributes=False):
+def load_fashion_mnist(return_X_y=False, split=False):
+    """Load and return the CIFAR10 Data Set features extracted using a
+    pretrained ResNet18 neural network (classification).
+    """
+    module_path = dirname(__file__)
+
+    df1 = pd.read_csv(join(module_path, 'data/embedding_datasets', 'fashion_mnist_features_resnet18_train_1.csv'), header=None, index_col=0)
+    df2 = pd.read_csv(join(module_path, 'data/embedding_datasets', 'fashion_mnist_features_resnet18_train_2.csv'), header=None, index_col=0)
+    df3 = pd.read_csv(join(module_path, 'data/embedding_datasets', 'fashion_mnist_features_resnet18_train_3.csv'), header=None, index_col=0)
+    df4 = pd.read_csv(join(module_path, 'data/embedding_datasets', 'fashion_mnist_features_resnet18_train_3.csv'), header=None, index_col=0)
+    df5 = pd.read_csv(join(module_path, 'data/embedding_datasets', 'fashion_mnist_features_resnet18_test.csv'), header=None, index_col=0)
+
+    dataset = np.array(pd.concat([df1, df2, df3, df4, df5]))
+    data = dataset[:, :-1]
+    target = dataset[:, -1]
+
+    trans = SimpleImputer(strategy='median')
+    data = trans.fit_transform(data)
+
+    target = normalizeLabels(target)
+    return data, target
+
+
+def load_yearbook(return_X_y=False, split=False):
     """Load and return the Yearbook Data Set features extracted using a
     pretrained ResNet18 neural network (classification).
 
@@ -283,74 +350,29 @@ def load_yearbook_features_resnet18(with_info=False, with_attributes=False):
     Dimensionality                          512
     Features                              float
     ===========================================
-
-    Parameters
-    ----------
-    with_info : boolean, default=False.
-        If True, returns ``(data, target)`` instead of a Bunch object.
-        See below for more information about the `data` and `target` object.
-
-    with_attributes : boolean, default=False.
-        If True, returns an additional dictionary containing information of
-        additional attributes: year, state, city, school of the portraits.
-        The key 'attr_labels' in the dictionary contains these labels
-        corresponding to each columns, while 'attr_data' corresponds to
-        the attribute data in form of numpy array.
-
     Returns
     -------
-    bunch : Bunch
-        Dictionary-like object, the interesting attributes are:
-        'data', the data to learn, 'target', the classification targets,
-        'DESCR', the full description of the dataset,
-        and 'filename', the physical location of Yearbook ResNet18 features
-        csv dataset.
-
     (data, target) : tuple if ``with_info`` is False
-
     """
+
     module_path = dirname(__file__)
 
-    fdescr_name = join(module_path, 'descr', 'yearbook_features_resnet18.rst')
-    with open(fdescr_name) as f:
-        descr_text = f.read()
+    df1 = pd.read_csv(join(module_path, 'data/embedding_datasets', 'yearbook_features_resnet18_1.csv'),
+                      header=None, index_col=0)
+    df2 = pd.read_csv(join(module_path, 'data/embedding_datasets', 'yearbook_features_resnet18_2.csv'),
+                      header=None, index_col=0)
+    df3 = pd.read_csv(join(module_path, 'data/embedding_datasets', 'yearbook_features_resnet18_3.csv'),
+                      header=None, index_col=0)
 
-    zf = zipfile.ZipFile(join(module_path, 'data',
-                              'yearbook_features_resnet18_1.csv.zip'))
-    df1 = pd.read_csv(zf.open('yearbook_features_resnet18_1.csv'), header=None)
-    zf = zipfile.ZipFile(join(module_path, 'data',
-                              'yearbook_features_resnet18_2.csv.zip'))
-    df2 = pd.read_csv(zf.open('yearbook_features_resnet18_2.csv'), header=None)
-
-    dataset = np.array(pd.concat([df1, df2]))
+    dataset = np.array(pd.concat([df1, df2, df3]))
     data = dataset[:, :-1]
     target = dataset[:, -1]
 
     trans = SimpleImputer(strategy='median')
     data = trans.fit_transform(data)
 
-    if with_attributes:
-        attr = pd.read_csv(join(module_path, 'data',
-                                'yearbook_attributes.csv'))
-        attr_labels = attr.columns.values
-        attr_val = attr.values
-        attr = {'attr_labels': attr_labels, 'attr_data': attr_val}
-
-        if not with_info:
-            return data, normalizeLabels(target), attr
-
-        return Bunch(data=data, target=normalizeLabels(target),
-                     attributes=attr, DESCR=descr_text)
-
-    else:
-        if not with_info:
-            return data, normalizeLabels(target)
-
-        return Bunch(data=data, target=normalizeLabels(target),
-                     DESCR=descr_text)
-
-
-
+    target = normalizeLabels(target)
+    return data, target
 
 
 def load_thyroid(return_X_y=False):
